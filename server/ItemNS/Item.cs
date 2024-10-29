5bfd8ad6170a41bc47643db7844178a534f2edc6
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2024-10-29 01:12:49 +0100

    Update Item.cs

--- server/ItemNS/Item.cs
+++ server/ItemNS/Item.cs
@@ -4,7 +4,7 @@ namespace GameInv.ItemNS {
         public readonly ItemDurability DamagePerUse;
         public readonly ItemDurability Durability = new();
         public string Name = name;
-        
+
         // todo: only store durability if needed (also store a bool), make them nullable and only define in the contructor if bool is true
     }
 }
