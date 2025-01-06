9038504616bc02aba1b7688c0e60156e330871e8
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2025-01-06 13:05:20 +0100

    Replace Bukkit with Spigot

--- minecraft-spigot-client/src/main/kotlin/xyz/mp3martin/gameInvSpigotClient/GameInvSpigotClient.kt
+++ minecraft-spigot-client/src/main/kotlin/xyz/mp3martin/gameInvSpigotClient/GameInvSpigotClient.kt
@@ -1,8 +1,8 @@
-package xyz.mp3martin.gameInvBukkitClient
+package xyz.mp3martin.gameInvSpigotClient
 
 import org.bukkit.plugin.java.JavaPlugin
 
-class GameInvBukkitClient : JavaPlugin() {
+class GameInvSpigotClient : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
